6ab42b55295eb2e848cb2d5efdc07fb3e207ad06
RM92023 <robinjara20@gmail.com>
2023-05-23 10:05:32 -0500

    Adding new project, Big O and sorting algoritms

---
+++ 0-bubble_sort.c
@@ -0,0 +1,50 @@
+#include "sort.h"
+/**
+ * swap - swap the elements in the method bubble
+ *
+ * @a: the first element
+ * @b: the second element
+ *
+ * Return: Helper function to swap two elements in the array
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+
+/**
+ * bubble_sort - sort elements in ascending order
+ *
+ * @array: array of elements sorted
+ * @size: size of array
+ *
+ * Return: Function to sort the array using Bubble sort algorithm
+ */
+void bubble_sort(int *array, size_t size)
+{
+	if (size <= 1)
+		return;
+
+	for (size_t i = 0; i < size - 1; i++)
+	{
+		for (size_t j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				/* Swap the elements if they are in the wrong order */
+				swap(&array[j], &array[j + 1]);
+			}
+		}
+
+		/* Print the array after each pass */
+		printf("Pass %zu: ", i + 1);
+		for (size_t k = 0; k < size; k++)
+		{
+			printf("%d ", array[k]);
+		}
+		printf("\n");
+	}
+}
